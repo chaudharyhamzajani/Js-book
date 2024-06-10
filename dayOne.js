ef8522db431976f09e282be23d9656285c95a103
Hamza <161723182+chaudharyhamzajani@users.noreply.github.com>
2024-06-10 23:04:03 +0500

    Add files via upload
    day one

---
+++ dayOne.js
@@ -0,0 +1,26 @@
+     //exercise 1.1
+//type 4+10 and enter in console browser??? what is the answer
+// Answer is 14
+
+     //exercise 1.2
+//create html file and print hello world in the browser
+console.log("hello world");
+
+     //exercise 1.3
+//1.link the javaScript file with html.
+// 2 check javaScript code run properly
+console.log("hello world"); 
+
+     //exercise 1.4
+//1.add a statement let a = 10 
+// 2.print it 
+// 3. comment a line what is the value of a
+let a = 10;
+console.log(a);
+//the value of variable is 10
+
+//chapter One project
+//create an html file and a linked JavaScript file
+//1.output your name
+//2.add multiline comment
+//3.comment out console.log message that nothing show in the console screen 
