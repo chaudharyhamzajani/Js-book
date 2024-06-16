4be0a8e2be55189bfd8690c30aed9554e446a803
Hamza <161723182+chaudharyhamzajani@users.noreply.github.com>
2024-06-17 00:11:50 +0500

    Add files via upload

--- day3/chapter4/first.js
+++ day3/chapter4/first.js
@@ -28,9 +28,39 @@
 // if(id == true ? console.log(`plese welcom`) : console.log(`sorry sir wrong id`));   //use ternary operator
 
 
-//4.4 exercise
-// In this exercise, we'll create a Magic 8-Ball random answer generator
-// let getValue = Math.random();
+// //4.4 exercise
+// // In this exercise, we'll create a Magic 8-Ball random answer generator
+// //in this we get a number from user b/w 0-5 and generate a random value 
+// //and print the result on the basis of random value.if user can't give 
+// //value its print try again.
+// let getValue = Math.random()*6;
+// Math.round(2).getValue
+// let finalOutput = prompt();
+// switch (finalOutput) {
+//     case "0":
+//         result = `you won the car`;
+//         break;
+//     case finalOutput = "1":
+//         console.log(`you won the bike`);
+//         break;
+//     case finalOutput = "2":
+//         console.log(`you won the dimond ring`);
+//         break;
+//     case finalOutput = "3":
+//         console.log(`you won the AC`);
+//         break;
+//     case finalOutput = "4":
+//         console.log(`you won the LED`);
+//         break;
+//     case finalOutput = "5":
+//         console.log( `you won the Mobile`);
+//         break;
+
+//     default:
+//        console.log(`try again`);
+//         break;             
+// }
+// let finalResult = console.log(`:- you select number  ${finalOutput}`);
 
 
 // //4.5 exercise
@@ -168,3 +198,39 @@
 // let check = (val % 2) ? `Odd` : `Even`;
 // check = `${val} is ${check}`;
 // console.log(check);        //100 is even
+
+
+
+// project
+//Rock Paper Scissors game
+// This is a game between a player and the computer, where both will make a random
+// selection of either Rock, Paper, or Scissors (alternatively, you could create a version
+// using real player input!). Rock will beat out Scissors, Paper will beat out Rock, and
+// Scissors will beat out Paper. You can use JavaScript to create your own version of
+// this game, applying the logic with an if statement.
+let userInput = 0;
+let mechineInput = 0;
+function playGame (){ 
+    const options = ["Rock" , "Paper" , "Scissors"];          //create array
+    const playerChoiceIndex = Math.floor(Math.random()*3);    //generate random selection for players
+    const computerChoiceIndex = Math.floor(Math.random()*3);
+    const playerChoice =  options[playerChoiceIndex];
+    const computerChoice =  options[computerChoiceIndex];
+    let resultMessage = ` Player chose ${playerChoice} . Computer chose ${computerChoice}. `;
+    //create variable that hold the response message
+    if(playerChoice == computerChoice){
+        resultMessage += ` It's a tie! `;
+    }else{
+        //apply game logic
+        if((playerChoice === "Rock" && computerChoice === "Scissors") 
+            || (playerChoice === "Paper" && computerChoice === "Rock")
+        || (playerChoice === "Scissors" && computerChoice === "Paper")
+     ){
+        resultMessage += " :-- Player win"
+     } else{
+        resultMessage += " :-- Computer win";
+     }
+    };
+    //output the result message
+    document.getElementById("result").innerText = resultMessage;
+    };
